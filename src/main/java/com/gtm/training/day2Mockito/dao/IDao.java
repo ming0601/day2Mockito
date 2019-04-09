ad4dfb249a8346e65b2d5f72fa15f57b5d649d65
ming0601 <gh13112010@>
2019-04-09 15:41:43 +0200

    Added Insert, Get and Delete Tests

--- src/main/java/com/gtm/training/day2Mockito/dao/IDao.java
+++ src/main/java/com/gtm/training/day2Mockito/dao/IDao.java
@@ -2,10 +2,15 @@ package com.gtm.training.day2Mockito.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.gtm.training.day2Mockito.model.Client;
 
+@Transactional
 @Repository
 public interface IDao extends JpaRepository<Client, Integer>{
 
+	public Client getByName(String name);
+	public void deleteByName(String name);
 	
 }
--- src/test/java/com/gtm/training/day2Mockito/dao/DaoTest.java
+++ src/test/java/com/gtm/training/day2Mockito/dao/DaoTest.java
@@ -1,6 +1,7 @@
 package com.gtm.training.day2Mockito.dao;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,11 @@ public class DaoTest {
 	
 	@Autowired
 	private IDao dao;
+	
+	@BeforeEach
+	void init() {
+		dao.save(new Client("Jean", "123"));
+	}
 		
 	@Test
 	public void testInsert() {		
@@ -18,4 +24,19 @@ public class DaoTest {
 		Client result = dao.save(client);		
 		Assertions.assertEquals(client, result);		
 	}
+	
+	@Test
+	void testGet() {
+		Client result = dao.getByName("Jean");
+		Assertions.assertEquals("Jean", result.getName());
+		Assertions.assertEquals("123", result.getAccountNumber());
+	}
+	
+	@Test
+	void testDelete() {
+		dao.deleteByName("Jean");
+		Client nullClient = dao.getByName("Jean");
+		Assertions.assertNull(nullClient);
+	}
+
 }
