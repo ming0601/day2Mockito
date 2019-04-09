4a927cc4eae3e816875e87dd17dfe04ab3d32840
ming0601 <gh13112010@>
2019-04-09 15:20:31 +0200

    Added H2 DB and JUnit5

--- src/main/java/com/gtm/training/day2Mockito/dao/IDao.java
+++ src/main/java/com/gtm/training/day2Mockito/dao/IDao.java
@@ -1,9 +1,7 @@
 package com.gtm.training.day2Mockito.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-
 import com.gtm.training.day2Mockito.model.Client;
 
 @Repository
--- src/test/java/com/gtm/training/day2Mockito/dao/DaoTest.java
+++ src/test/java/com/gtm/training/day2Mockito/dao/DaoTest.java
@@ -1,36 +1,21 @@
 package com.gtm.training.day2Mockito.dao;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import org.springframework.boot.test.context.SpringBootTest;
 import com.gtm.training.day2Mockito.model.Client;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class DaoTest {
-
 	
 	@Autowired
-	private static IDao	dao;
-	
-//	@BeforeAll
-//	public static void initialisation() {
-//		
-//		dao = new IDao();
-//		
-//	}
-	
+	private IDao dao;
+		
 	@Test
-	public void testInsert() {
-		
-		Client client = new Client("Roger", "000015");
-		
-		Client result = dao.save(client);
-		
-		Assertions.assertEquals(client, result);
-		
+	public void testInsert() {		
+		Client client = new Client("Roger", "000015");		
+		Client result = dao.save(client);		
+		Assertions.assertEquals(client, result);		
 	}
 }
