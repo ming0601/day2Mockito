d3a3ae7c42c8aa34cf765854e7f6fc7a577d55bb
ming0601 <gh13112010@>
2019-04-09 12:29:36 +0200

    first commit

---
+++ src/main/java/com/gtm/training/day2Mockito/Day2MockitoApplication.java
@@ -0,0 +1,13 @@
+package com.gtm.training.day2Mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2MockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2MockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtm/training/day2Mockito/Day2MockitoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gtm.training.day2Mockito;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Day2MockitoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
